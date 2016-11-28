f2ba1adea96024e39d4f3b266119c678945e2f9e
Ryan West <rbwest@ncsu.edu>
2016-11-28 13:22:35 -0500

    Made several changes to bring project closer to rails tutorial (use rails to pass data and draw graph). Currently having issue with routes.

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,16 @@ QuickSearch::Engine.routes.draw do
   match 'appstats/detail/:ga_scope', to: 'appstats#detail', via: [:get, :post]
   get 'appstats/realtime' => 'appstats#realtime'
 
+  ########################## ADDED #############################
+  
+  # FROM TUTORIAL
+  #get 'graph/index'
+  #get 'graph/data', :defaults => { :format => 'json' }
+
+  get 'graph/data', :defaults => { :format => 'json' }
+
+  ##############################################################
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
