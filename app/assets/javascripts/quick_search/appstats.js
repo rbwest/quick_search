8e8bcd664682c41705d7d220773178650f232b7b
Ryan West <rbwest@ncsu.edu>
2016-11-30 14:32:50 -0500

    Updated visualization

--- app/assets/javascripts/quick_search/appstats.js
+++ app/assets/javascripts/quick_search/appstats.js
@@ -8,7 +8,7 @@ $(function () {
 });
 
 /*                RENDER SAMPLE GRAPH              */
-$.ajax({
+$(function() { $.ajax({
            type: "GET",
            contentType: "application/json; charset=utf-8",
            url: 'appstats/data',
@@ -20,17 +20,19 @@ $.ajax({
                error();
            }
        });
+});
 
 function draw(data) {
 	console.log(data);
 	var svg = d3.select("#genGraph"),
 	    margin = {top: 20, right: 20, bottom: 20, left: 20},
 	    width = +svg.attr("width") - margin.left - margin.right,
-	    height = +svg.attr("height") - margin.top - margin.bottom;
+	    height = +svg.attr("height") - margin.top - margin.bottom
+	    labelWidth = 100;
 
-	var x = d3.scaleLinear().rangeRound([0, width]),
-	    y = d3.scaleLinear().rangeRound([0, height])
-	    yL = d3.scaleLinear().rangeRound([0, height + 25]);
+	var x = d3.scaleLinear().rangeRound([0, width - margin.left - margin.right - labelWidth]),
+	    y = d3.scaleLinear().rangeRound([0, height - margin.top - margin.bottom - 26])
+	    yL = d3.scaleLinear().rangeRound([0, height - margin.top - margin.bottom]);
 
 	var g = svg.append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -38,32 +40,38 @@ function draw(data) {
 	  x.domain([0, d3.max(data, function(d) { return d.clickcount; })]);
 	  y.domain([0, d3.max(data, function(d , i) { return i; })]);
 
-	// g.append("g")
-	//   .attr("class", "axis axis--x")
-	//   .attr("transform", "translate(0," + height + ")")
-	//   .call(d3.axisBottom(x).ticks(7));
+	 g.append("g")
+	   .attr("class", "axis axis--x")
+	   .attr("transform", "translate(" + labelWidth + ",50)")
+	   .call(d3.axisTop(x).ticks( d3.max(data, function(d) { return d.clickcount; })) );
 
 	g.append("g")
 	  .attr("class", "axis axis--y")
-	  .attr("transform", "translate(" + 100 + "," + 0 + ")")
+	  .attr("transform", "translate(" + labelWidth + ",50)")
       .call(d3.axisLeft(yL).ticks(0));
-	//   .append("text")
-	//   .attr("transform", "rotate(-90)")
-	//   .attr("y", 6)
-	//   .attr("dy", "0.71em")
-	//   .attr("text-anchor", "end")
-	//   .text("Frequency");
 
-	g.selectAll(".bar")
-	.data(data)
-	.enter().append("rect")
+	g.append("g")
+	  .attr("class" , "data bars")
+	  .attr("transform", "translate(" + (labelWidth+2) + ",52)")
+	  .selectAll(".bar")
+	  .data(data)
+	  .enter().append("rect")
 	  .attr("class", "bar")
-	  .attr("x", 100 )
+	  .attr("x", 0 )
 	  .attr("y", function(d , i) { return y(i); })
 	  .attr("width", function(d) { return x(d.clickcount); })
 	  .attr("height", 25 );
 
-	g.selectAll(".label")
+	g.append("g")
+	  .attr("class" , "labels labels--x")
+	  .attr("transform", "translate(" + (width/2 - 14) + ",30)")
+	  .append("text")
+	  .text("Click Count");
+
+	g.append("g")
+	  .attr("class" , "labels labels--y")
+	  .attr("transform", "translate(0,50)")
+	  .selectAll(".label")
 	  .data(data)
 	  .enter().append("text")
 	  .text( function (d) { return d.action })
